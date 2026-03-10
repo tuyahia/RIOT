b8718e44e58e8b32f1b2f3e71565fd5c513879f6
Yahia Abdella <yahia.abdella@tuhh.de>
2026-03-10 11:11:23 +0100

    drivers/gc9a01a: add auto init for the GC9A01A display controller

---
+++ sys/auto_init/screen/auto_init_gc9a01a.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2020 Technische Universität Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_auto_init
+ * @{
+ * @file
+ * @brief       initializes lcd display device
+ *
+ * @author      Yahia Abdella <yahia.abdella@tuhh.de>
+ * @}
+ */
+
+#include <assert.h>
+#include <stddef.h>
+
+#include "log.h"
+
+#include "disp_dev.h"
+
+#include "lcd.h"
+#include "lcd_disp_dev.h"
+
+#include "gc9a01a.h"
+#include "gc9a01a_params.h"
+
+#ifndef GC9A01A_NUMOF
+#define GC9A01A_NUMOF               0
+#endif
+#ifndef GC9A01A_SCREEN_NUMOF
+#define GC9A01A_SCREEN_NUMOF        0
+#endif
+
+static gc9a01a_t gc9a01a_devs[GC9A01A_NUMOF];
+
+static disp_dev_reg_t disp_dev_entries[GC9A01A_NUMOF];
+
+void auto_init_gc9a01a(void)
+{
+    assert(GC9A01A_NUMOF == GC9A01A_SCREEN_NUMOF);
+
+    for (size_t i = 0; i < GC9A01A_NUMOF; i++) {
+        gc9a01a_devs[i].dev.driver = &lcd_gc9a01a_driver;
+        LOG_DEBUG("[auto_init_screen] initializing gc9a01a #%u\n", i);
+        if (lcd_init(&gc9a01a_devs[i].dev, &gc9a01a_params[i]) < 0) {
+            LOG_ERROR("[auto_init_screen] error initializing gc9a01a #%u\n", i);
+            continue;
+        }
+
+        disp_dev_entries[i].dev = (disp_dev_t *) &gc9a01a_devs[i].dev;
+        disp_dev_entries[i].screen_id = gc9a01a_screen_ids[i];
+        disp_dev_entries[i].dev->driver = &lcd_disp_dev_driver;
+
+        /* add to disp_dev registry */
+        disp_dev_reg_add(&(disp_dev_entries[i]));
+    }
+}
--- sys/auto_init/screen/init.c
+++ sys/auto_init/screen/init.c
@@ -41,6 +41,10 @@ void auto_init_screen(void)
             extern void auto_init_u8g2_display(void);
             auto_init_u8g2_display();
         }
+        if (IS_USED(MODULE_GC9A01A)) {
+            extern void auto_init_gc9a01a(void);
+            auto_init_gc9a01a();
+        }
     }
 
     if (IS_USED(MODULE_TOUCH_DEV)) {
